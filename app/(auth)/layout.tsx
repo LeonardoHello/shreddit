cf1e7798a30876abed9d5da6e8218b01a2282694
Leonardo Yakub <leonardo.yakub@gmail.com>
2023-11-05 18:30:07 +0100

    added redirect url prop for auth components

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,6 +4,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid min-h-screen place-content-center">{children}</div>
+    <div className="grid min-h-screen place-content-center bg-zinc-950 p-12">
+      {children}
+    </div>
   );
 }
