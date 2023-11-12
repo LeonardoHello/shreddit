081c196dd7f36cb1b572368c7013825f8b948f31
Leonardo Yakub <leonardo.yakub@gmail.com>
2023-11-12 06:27:32 +0100

    created clerks user components for signing out and editing users information

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,11 +1,16 @@
 import CurrentFeed from "@/components/CurrentFeed";
 
+import UserProfile from "./UserProfile";
+
 export default async function UserInfo({ userId }: { userId: string }) {
   if (userId === null) throw new Error("Couldn't load user information");
 
   return (
-    <CurrentFeed>
-      <div>UserInfo</div>
-    </CurrentFeed>
+    <>
+      <CurrentFeed>
+        <div>UserInfo</div>
+      </CurrentFeed>
+      <UserProfile />
+    </>
   );
 }
---
+++ components/UserProfile.tsx
@@ -0,0 +1,22 @@
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { UserIcon } from "@heroicons/react/24/solid";
+
+export default function UserProfile() {
+  return (
+    <div className="order-2 ml-auto self-center">
+      <ClerkLoading>
+        <UserIcon className="h-8 w-8 animate-pulse rounded-full bg-zinc-300 p-0.5 text-zinc-800 opacity-80" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              userButtonPopoverCard: "border border-zinc-700/70",
+            },
+          }}
+        />
+      </ClerkLoaded>
+    </div>
+  );
+}
