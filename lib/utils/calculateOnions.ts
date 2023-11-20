517b8e8beeae36e14bb0d44119678a8fc1dcc1b9
Leonardo Yakub <leonardo.yakub@gmail.com>
2023-11-20 05:25:44 +0100

    calculating onions only based on upvotes and joined members

--- lib/utils/calculateOnions.ts
+++ lib/utils/calculateOnions.ts
@@ -3,9 +3,6 @@ import { RouterOutput } from "@/trpc/server/procedures";
 export default function getOnions(user: RouterOutput["searchUsers"][0]) {
   let onions = 0;
 
-  // +2 onions for each comment
-  onions += user.comments.length * 2;
-
   // +1 onion for each upvoted comment
   user.comments.forEach((comment) => {
     let upvotes = 0;
@@ -17,9 +14,6 @@ export default function getOnions(user: RouterOutput["searchUsers"][0]) {
     }
   });
 
-  // +3 onions for each post
-  onions += user.posts.length * 3;
-
   // +1 onion for each upvoted post
   user.posts.forEach((post) => {
     let upvotes = 0;
@@ -30,9 +24,6 @@ export default function getOnions(user: RouterOutput["searchUsers"][0]) {
       onions += upvotes;
     }
   });
-
-  // +5 onions for each community
-  onions += user.usersToCommunities.length * 5;
 
   user.usersToCommunities.forEach((moderatedCommunities) => {
     // +1 onion for each community member
