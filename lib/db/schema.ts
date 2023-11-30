bfa59602675a907e3880444a7503f1e955e561fe
Leonardo Yakub <leonardo.yakub@gmail.com>
2023-11-30 23:35:27 +0100

    created dedicated many-to-many relationship tables for post and comment upvotes

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,5 +1,6 @@
-import { InferSelectModel, relations } from "drizzle-orm";
+import { type InferSelectModel, relations } from "drizzle-orm";
 import {
+  type AnyPgColumn,
   boolean,
   index,
   pgTable,
@@ -24,9 +25,12 @@ export const users = pgTable(
 );
 
 export const usersRelations = relations(users, ({ many }) => ({
+  communities: many(communities),
   usersToCommunities: many(usersToCommunities),
   posts: many(posts),
+  usersToPosts: many(usersToPosts),
   comments: many(comments),
+  usersToComments: many(usersToComments),
 }));
 
 export const communities = pgTable(
@@ -38,15 +42,24 @@ export const communities = pgTable(
     imageUrl: text("image_url"),
     about: text("about"),
     nsfw: boolean("nsfw").notNull().default(false),
+    moderatorId: text("moderator_id")
+      .references(() => users.id, {
+        onDelete: "cascade",
+      })
+      .notNull(),
   },
   (t) => ({
     nameIdx: uniqueIndex("name_idx").on(t.name),
   }),
 );
 
-export const communitiesRelations = relations(communities, ({ many }) => ({
-  usersToCommunities: many(usersToCommunities),
+export const communitiesRelations = relations(communities, ({ one, many }) => ({
+  moderator: one(users, {
+    fields: [communities.moderatorId],
+    references: [users.id],
+  }),
   posts: many(posts),
+  usersToCommunities: many(usersToCommunities),
 }));
 
 export const usersToCommunities = pgTable(
@@ -61,10 +74,9 @@ export const usersToCommunities = pgTable(
     muted: boolean("muted").notNull().default(false),
     favorite: boolean("favorite").notNull().default(false),
     member: boolean("member").notNull().default(true),
-    author: boolean("author").notNull().default(false),
   },
   (t) => ({
-    pk: primaryKey(t.userId, t.communityId),
+    pk: primaryKey({ columns: [t.userId, t.communityId] }),
     userIdIdx: index("user_id_idx").on(t.userId),
   }),
 );
@@ -91,8 +103,6 @@ export const posts = pgTable("posts", {
   text: text("text"),
   nsfw: boolean("nsfw").notNull().default(false),
   spoiler: boolean("spoiler").notNull().default(false),
-  upvoted: text("upvoted").array(),
-  downvoted: text("downvoted").array(),
   authorId: text("author_id")
     .references(() => users.id, {
       onDelete: "cascade",
@@ -114,6 +124,39 @@ export const postsRelations = relations(posts, ({ one, many }) => ({
   }),
   files: many(files),
   comments: many(comments),
+  usersToPosts: many(usersToPosts),
+}));
+
+export const usersToPosts = pgTable(
+  "users_to_posts",
+  {
+    userId: text("user_id")
+      .references(() => users.id, { onDelete: "cascade" })
+      .notNull(),
+    postId: uuid("post_id")
+      .references(() => posts.id, { onDelete: "cascade" })
+      .notNull(),
+    upvoted: boolean("upvoted").notNull().default(false),
+    downvoted: boolean("downvoted").notNull().default(false),
+    saved: boolean("saved").notNull().default(false),
+    hidden: boolean("hidden").notNull().default(false),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.userId, t.postId] }),
+    userIdIdx: index("user_id_idx").on(t.userId),
+    postIdIdx: index("post_id_idx").on(t.postId),
+  }),
+);
+
+export const usersToPostsRelations = relations(usersToPosts, ({ one }) => ({
+  user: one(users, {
+    fields: [usersToPosts.userId],
+    references: [users.id],
+  }),
+  post: one(posts, {
+    fields: [usersToPosts.postId],
+    references: [posts.id],
+  }),
 }));
 
 export const files = pgTable("files", {
@@ -138,9 +181,12 @@ export const comments = pgTable("comments", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at"),
   text: text("text").notNull(),
-  upvoted: text("upvoted").array(),
-  downvoted: text("downvoted").array(),
-  replyId: uuid("reply_id"),
+  parentCommentId: uuid("parent_comment_id").references(
+    (): AnyPgColumn => comments.id,
+    {
+      onDelete: "cascade",
+    },
+  ),
   authorId: text("author_id")
     .references(() => users.id, {
       onDelete: "cascade",
@@ -151,7 +197,7 @@ export const comments = pgTable("comments", {
     .notNull(),
 });
 
-export const commentsRelations = relations(comments, ({ one }) => ({
+export const commentsRelations = relations(comments, ({ one, many }) => ({
   post: one(posts, {
     fields: [comments.postId],
     references: [posts.id],
@@ -160,22 +206,62 @@ export const commentsRelations = relations(comments, ({ one }) => ({
     fields: [comments.authorId],
     references: [users.id],
   }),
-  reply: one(comments, {
-    fields: [comments.replyId],
+  parentComment: one(comments, {
+    fields: [comments.parentCommentId],
     references: [comments.id],
   }),
+  usersToComments: many(usersToComments),
 }));
+
+export const usersToComments = pgTable(
+  "users_to_comments",
+  {
+    userId: text("user_id")
+      .references(() => users.id, { onDelete: "cascade" })
+      .notNull(),
+    commentId: uuid("comment_id")
+      .references(() => comments.id, { onDelete: "cascade" })
+      .notNull(),
+    upvoted: boolean("upvoted").notNull().default(false),
+    downvoted: boolean("downvoted").notNull().default(false),
+    saved: boolean("saved").notNull().default(false),
+    hidden: boolean("hidden").notNull().default(false),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.userId, t.commentId] }),
+    userIdIdx: index("user_id_idx").on(t.userId),
+    commentIdIdx: index("comment_id_idx").on(t.commentId),
+  }),
+);
+
+export const usersToCommentsRelations = relations(
+  usersToComments,
+  ({ one }) => ({
+    user: one(users, {
+      fields: [usersToComments.userId],
+      references: [users.id],
+    }),
+    comment: one(comments, {
+      fields: [usersToComments.commentId],
+      references: [comments.id],
+    }),
+  }),
+);
 
 export type User = InferSelectModel<typeof users>;
 export type Community = InferSelectModel<typeof communities>;
 export type UserToCommunity = InferSelectModel<typeof usersToCommunities>;
 export type Post = InferSelectModel<typeof posts>;
+export type UserToPost = InferSelectModel<typeof usersToPosts>;
 export type File = InferSelectModel<typeof files>;
 export type Comment = InferSelectModel<typeof comments>;
+export type UserToComments = InferSelectModel<typeof usersToComments>;
 
 export const UserSchema = createSelectSchema(users);
 export const CommunitySchema = createSelectSchema(communities);
 export const UserToCommunitySchema = createSelectSchema(usersToCommunities);
 export const PostSchema = createSelectSchema(posts);
+export const UserToPostSchema = createSelectSchema(usersToPosts);
 export const FileSchema = createSelectSchema(files);
 export const CommentSchema = createSelectSchema(comments);
+export const UserToCommentSchema = createSelectSchema(usersToComments);
