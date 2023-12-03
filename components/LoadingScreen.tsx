3bc806d69d513a916a27d1c466df8bf19a0621a5
Leonardo Yakub <leonardo.yakub@gmail.com>
2023-12-04 00:58:28 +0100

    loading container recolor

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,7 +1,7 @@
 export default function LoadingScreen() {
   return (
     <div className="grid min-h-screen flex-grow place-content-center">
-      <div className="h-24 w-24 animate-spin rounded-full border-8 border-zinc-700 border-t-rose-500" />
+      <div className="h-24 w-24 animate-spin rounded-full border-8 border-zinc-700/70 border-t-rose-500" />
     </div>
   );
 }
