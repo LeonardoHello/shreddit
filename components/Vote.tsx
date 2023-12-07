3540f45711ac797c7d62d9a77b8ea1ffd36f969b
Leonardo Yakub <leonardo.yakub@gmail.com>
2023-12-07 20:46:58 +0100

    memoizing Vote component and optimistically updating upvotes

--- components/Vote.tsx
+++ components/Vote.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 import {
   ArrowDownCircleIcon,
   ArrowUpCircleIcon,
@@ -6,22 +8,38 @@ import { toast } from "sonner";
 
 import { UserToPost } from "@/lib/db/schema";
 import cn from "@/lib/utils/cn";
-import { RouterOutput } from "@/trpc/procedures";
+import type { RouterInput, RouterOutput } from "@/trpc/procedures";
 import { trpc } from "@/trpc/react";
-import type { ArrElement } from "@/types";
+import { ArrElement } from "@/types";
 
 type Props = {
-  post: ArrElement<RouterOutput["joinedCommunitiesPosts"]["posts"]>;
-  userId: string | null | undefined;
+  usersToPosts: ArrElement<
+    RouterOutput["joinedCommunitiesPosts"]["posts"]
+  >["usersToPosts"];
+  userId: UserToPost["userId"];
+  postId: UserToPost["postId"];
 };
 
-export default function Vote({ post, userId }: Props) {
+const Vote = memo(function Vote({ usersToPosts, userId, postId }: Props) {
   const utils = trpc.useUtils();
 
-  const onSuccess = async (_data: UserToPost[]) => {
-    await utils.joinedCommunitiesPosts.cancel();
+  const userToPost = usersToPosts.findLast((userToPost) => {
+    return userToPost.userId === userId;
+  });
 
-    const userToPost = _data[0];
+  const upvoteCount = usersToPosts.reduce((a, b) => {
+    return b.upvoted ? a + 1 : b.downvoted ? a - 1 : a;
+  }, 0);
+
+  const onError = async ({ message }: { message: string }) => {
+    toast.error(message);
+    await utils.joinedCommunitiesPosts.refetch({}, {}, { throwOnError: true });
+  };
+
+  const onMutate = async (
+    variables: RouterInput["upvotePost" | "downvotePost"],
+  ) => {
+    await utils.joinedCommunitiesPosts.cancel();
 
     utils.joinedCommunitiesPosts.setInfiniteData({}, (data) => {
       if (!data) {
@@ -38,18 +56,30 @@ export default function Vote({ post, userId }: Props) {
         pages: data.pages.map((page) => ({
           ...page,
           posts: page.posts.map((_post) => {
-            if (_post.id !== post.id) return _post;
+            if (_post.id !== variables.postId) return _post;
 
             let usersToPosts = structuredClone(_post.usersToPosts);
 
-            const index = usersToPosts.findLastIndex(
-              (userToPost) => userToPost.userId === userId,
-            );
-
-            if (index === -1) {
-              usersToPosts.push(userToPost);
+            if (!userToPost) {
+              usersToPosts.push({
+                upvoted: false,
+                downvoted: false,
+                hidden: false,
+                saved: false,
+                userId,
+                ...variables,
+              });
             } else {
-              usersToPosts = usersToPosts.with(index, userToPost);
+              const index = usersToPosts.findLastIndex(
+                (_userToPost) => _userToPost.userId === userId,
+              );
+
+              usersToPosts = usersToPosts.with(index, {
+                ...userToPost,
+                upvoted: false,
+                downvoted: false,
+                ...variables,
+              });
             }
 
             return {
@@ -62,63 +92,55 @@ export default function Vote({ post, userId }: Props) {
     });
   };
 
-  const upvote = trpc.upvotePost.useMutation({
-    onError: ({ message }) => {
-      toast.error(message);
-    },
-    onSuccess,
-  });
-  const downvote = trpc.downvotePost.useMutation({
-    onError: ({ message }) => {
-      toast.error(message);
-    },
-    onSuccess,
-  });
-
-  const userToPost = post.usersToPosts.find(
-    (userToPost) => userToPost.userId === userId,
-  );
-
-  const upvoteCount = post.usersToPosts.reduce((a, b) => {
-    return b.upvoted ? a + 1 : b.downvoted ? a - 1 : a;
-  }, 0);
+  const upvote = trpc.upvotePost.useMutation({ onError, onMutate });
+  const downvote = trpc.downvotePost.useMutation({ onError, onMutate });
 
   return (
-    <div className="flex flex-col gap-0.5 items-center text-zinc-500 select-none">
+    <div className="flex select-none flex-col items-center gap-0.5 text-zinc-500">
       <ArrowUpCircleIcon
-        className={cn("h-8 w-8 hover:bg-zinc-700/50 rounded ", {
-          "text-rose-500": userToPost?.upvoted,
-        })}
+        className={cn(
+          "h-8 w-8 rounded transition-colors duration-300 hover:bg-zinc-700/50",
+          {
+            "text-rose-500": userToPost?.upvoted,
+          },
+        )}
         onClick={(e) => {
           e.preventDefault();
 
           upvote.mutate({
-            postId: post.id,
+            postId,
             upvoted: userToPost ? !userToPost.upvoted : true,
           });
         }}
       />
       <div
-        className={cn("text-xs font-bold text-zinc-300", {
-          "text-rose-500": userToPost?.upvoted,
-          "text-blue-500": userToPost?.downvoted,
-        })}
+        className={cn(
+          "text-xs font-bold text-zinc-300 transition-colors duration-300",
+          {
+            "text-rose-500": userToPost?.upvoted,
+            "text-blue-500": userToPost?.downvoted,
+          },
+        )}
       >
         {upvoteCount}
       </div>
       <ArrowDownCircleIcon
-        className={cn("h-8 w-8 hover:bg-zinc-700/50 rounded ", {
-          "text-blue-500": userToPost?.downvoted,
-        })}
+        className={cn(
+          "h-8 w-8 rounded transition-colors duration-300 hover:bg-zinc-700/50",
+          {
+            "text-blue-500": userToPost?.downvoted,
+          },
+        )}
         onClick={(e) => {
           e.preventDefault();
 
           downvote.mutate({
-            postId: post.id,
+            postId,
             downvoted: userToPost ? !userToPost.downvoted : true,
           });
         }}
       />
     </div>
   );
-}
+});
+export default Vote;
