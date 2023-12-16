212e7597559b647d64a6a95711361bc3f2a0a906
Leonardo Yakub <leonardo.yakub@gmail.com>
2023-12-16 13:23:29 +0100

    recalculated onions because of changed vote schema type

--- lib/utils/calculateOnions.ts
+++ lib/utils/calculateOnions.ts
@@ -2,23 +2,46 @@ import { RouterOutput } from "@/trpc/procedures";
 import type { ArrElement } from "@/types";
 
 export default function getOnions(
-  user: ArrElement<RouterOutput["searchUsers"]>,
+  user: ArrElement<RouterOutput["search"]["users"]>,
 ) {
   // +1 onion for each upvoted comment
-  const commentUpvotes = user.usersToComments.reduce((a, b) => {
-    return b.upvoted ? a + 1 : b.downvoted ? a - 1 : a;
-  }, 0);
+  const commentUpvotes = user.usersToComments.reduce(
+    (accumulator, currentValue) => {
+      return (
+        accumulator +
+        (currentValue.voteStatus === "upvoted"
+          ? 1
+          : currentValue.voteStatus === "downvoted"
+          ? -1
+          : 0)
+      );
+    },
+    0,
+  );
 
   // +1 onion for each upvoted post
-  const commentAndPostUpvotes = user.usersToPosts.reduce((a, b) => {
-    return b.upvoted ? a + 1 : b.downvoted ? a - 1 : a;
-  }, commentUpvotes);
+  const commentAndPostUpvotes = user.usersToPosts.reduce(
+    (accumulator, currentValue) => {
+      return (
+        accumulator +
+        (currentValue.voteStatus === "upvoted"
+          ? 1
+          : currentValue.voteStatus === "downvoted"
+          ? -1
+          : 0)
+      );
+    },
+    commentUpvotes,
+  );
 
   // +1 onion for each community member
-  const totalUpvotesAndMembers = user.communities.reduce((a, b) => {
-    // length is the count of members from my communities
-    return a + b.usersToCommunities.length;
-  }, commentAndPostUpvotes);
+  const totalUpvotesAndMembers = user.communities.reduce(
+    (accumulator, currentValue) => {
+      // length is the count of members from my communities
+      return accumulator + currentValue.usersToCommunities.length;
+    },
+    commentAndPostUpvotes,
+  );
 
-  return totalUpvotesAndMembers || 0;
+  return totalUpvotesAndMembers >= 0 ? totalUpvotesAndMembers : 0;
 }
