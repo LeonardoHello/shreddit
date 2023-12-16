5218fd77f7cd0c1372c92d66a772e15ff0582334
Leonardo Yakub <leonardo.yakub@gmail.com>
2023-12-16 12:52:03 +0100

    fixed sorted posts link routes

--- components/FeedSort.tsx
+++ components/FeedSort.tsx
@@ -18,34 +18,45 @@ import {
 
 import cn from "@/lib/utils/cn";
 
-export default function Sort({ path }: { path: string }) {
+export default function FeedSort() {
   const pathname = usePathname();
 
+  const path = pathname.replace(/\/(|best|new|hot|controversial)$/, "");
+
+  const defaultSort =
+    pathname.endsWith("/best") ||
+    !(
+      pathname.endsWith("/best") ||
+      pathname.endsWith("/hot") ||
+      pathname.endsWith("/new") ||
+      pathname.endsWith("/controversial")
+    );
+
   return (
-    <nav className="rounded border border-zinc-700/70 bg-zinc-900 p-3 text-sm lg:text-base">
+    <nav className="rounded border border-zinc-700/70 bg-zinc-900 p-3 text-base">
       <ul className="flex items-center gap-3 font-bold text-zinc-500">
         <li>
           <Link
-            href={`${path}best`}
+            href={`${path}/best`}
             className={cn(
               "flex items-center gap-1.5 rounded-full px-2 py-1.5 hover:bg-zinc-700/30",
               {
                 "bg-zinc-700/30 text-zinc-300 hover:bg-zinc-700/50":
-                  pathname.endsWith("/best") || pathname === path,
+                  defaultSort,
               },
             )}
           >
-            {pathname.endsWith("/best") || pathname === path ? (
-              <SolidRocketLaunchIcon className="h-5 w-5 lg:h-6 lg:w-6" />
+            {defaultSort ? (
+              <SolidRocketLaunchIcon className="h-6 w-6" />
             ) : (
-              <RocketLaunchIcon className="h-5 w-5 lg:h-6 lg:w-6" />
+              <RocketLaunchIcon className="h-6 w-6" />
             )}
-            Best
+            <span className="hidden sm:inline-block">Best</span>
           </Link>
         </li>
         <li>
           <Link
-            href={`${path}hot`}
+            href={`${path}/hot`}
             className={cn(
               "flex items-center gap-1.5 rounded-full px-2 py-1.5 hover:bg-zinc-700/30",
               {
@@ -55,16 +66,16 @@ export default function Sort({ path }: { path: string }) {
             )}
           >
             {pathname.endsWith("/hot") ? (
-              <SolidFireIcon className="h-5 w-5 lg:h-6 lg:w-6" />
+              <SolidFireIcon className="h-6 w-6" />
             ) : (
-              <FireIcon className="h-5 w-5 lg:h-6 lg:w-6" />
+              <FireIcon className="h-6 w-6" />
             )}
-            Hot
+            <span className="hidden sm:inline-block">Hot</span>
           </Link>
         </li>
         <li>
           <Link
-            href={`${path}new`}
+            href={`${path}/new`}
             className={cn(
               "flex items-center gap-1.5 rounded-full px-2 py-1.5 hover:bg-zinc-700/30",
               {
@@ -74,16 +85,16 @@ export default function Sort({ path }: { path: string }) {
             )}
           >
             {pathname.endsWith("/new") ? (
-              <SolidTagIcon className="h-5 w-5 lg:h-6 lg:w-6" />
+              <SolidTagIcon className="h-6 w-6" />
             ) : (
-              <TagIcon className="h-5 w-5 lg:h-6 lg:w-6" />
+              <TagIcon className="h-6 w-6" />
             )}
-            New
+            <span className="hidden sm:inline-block">New</span>
           </Link>
         </li>
         <li>
           <Link
-            href={`${path}controversial`}
+            href={`${path}/controversial`}
             className={cn(
               "flex items-center gap-1.5 rounded-full px-2 py-1.5 hover:bg-zinc-700/30",
               {
@@ -93,11 +104,11 @@ export default function Sort({ path }: { path: string }) {
             )}
           >
             {pathname.endsWith("/controversial") ? (
-              <SolidChatBubbleBottomCenterTextIcon className="h-5 w-5 lg:h-6 lg:w-6" />
+              <SolidChatBubbleBottomCenterTextIcon className="h-6 w-6" />
             ) : (
-              <ChatBubbleBottomCenterTextIcon className="h-5 w-5 lg:h-6 lg:w-6" />
+              <ChatBubbleBottomCenterTextIcon className="h-6 w-6" />
             )}
-            Controversial
+            <span className="hidden sm:inline-block">Controversial</span>
           </Link>
         </li>
       </ul>
