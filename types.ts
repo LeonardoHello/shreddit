7b332f7482c757e1fe1990f94f5afc7c706293c5
Leonardo Yakub <leonardo.yakub@gmail.com>
2023-12-16 12:44:14 +0100

    created types for accessing infinite query posts

--- types.ts
+++ types.ts
@@ -1,2 +1,19 @@
+import type { RouterInput, RouterOutput } from "@/trpc/procedures";
+
 export type ArrElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+export type InfinteQueryPostsProcedure = {
+  [K in keyof RouterOutput["posts"]]: "nextCursor" extends keyof RouterOutput["posts"][K]
+    ? K
+    : never;
+}[keyof RouterOutput["posts"]];
+
+export type InfinteQueryInfo<T extends InfinteQueryPostsProcedure> = {
+  procedure: T;
+  input: RouterInput["posts"][T];
+};
+
+export type InfinteQueryPost = ArrElement<
+  RouterOutput["posts"][InfinteQueryPostsProcedure]["posts"]
+>;
