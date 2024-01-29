52d1a0ba0169370b361650fb9c03d744df5a82c8
Leonardo Yakub <leonardo.yakub@gmail.com>
2024-01-29 11:13:05 +0100

    created seperate component for comment metadata

--- components/comment/Comment.tsx
+++ components/comment/Comment.tsx
@@ -2,15 +2,9 @@
 
 import { useState } from "react";
 
-import Image from "next/image";
-import Link from "next/link";
-
 import CommentContextProvider from "@/lib/context/CommentContextProvider";
 import type { User } from "@/lib/db/schema";
-import useHydration from "@/lib/hooks/useHydration";
 import type { ArrElement } from "@/lib/types";
-import getRelativeTimeString from "@/lib/utils/getRelativeTimeString";
-import dot from "@/public/dot.svg";
 import type { RouterOutput } from "@/trpc/procedures";
 import { trpc } from "@/trpc/react";
 
@@ -18,6 +12,7 @@ import CommentContentRTE from "../RTE/CommentContentRTE";
 import CommentReplyRTE from "../RTE/CommentReplyRTE";
 import CommentActions from "./CommentActions";
 import CommentActionsDropdown from "./CommentActionsDropdown";
+import CommentMetadata from "./CommentMetadata";
 
 export default function Comment({
   currentUserId,
@@ -39,8 +34,6 @@ export default function Comment({
   const [reply, setReply] = useState(false);
   const [editable, setEditable] = useState(false);
 
-  const hydrated = useHydration();
-
   const toggleEdit = () => setEditable((prev) => !prev);
   const cancelEdit = () => setEditable(false);
   const toggleReply = () => setReply((prev) => !prev);
@@ -49,41 +42,7 @@ export default function Comment({
   return (
     <CommentContextProvider comment={comment}>
       <div className="flex flex-col gap-2">
-        <div className="flex items-center gap-1 text-xs">
-          <Link href={`/u/${comment.author.name}`} className="rounded-full">
-            <Image
-              src={comment.author.imageUrl}
-              alt="user background"
-              priority
-              width={28}
-              height={28}
-              className="rounded-full"
-            />
-          </Link>
-          <Link
-            href={`/u/${comment.author.name}`}
-            className="font-medium hover:underline"
-          >
-            {comment.author.name}
-          </Link>
-
-          {comment.authorId === comment.post.authorId && (
-            <div className="font-bold uppercase text-blue-500">op</div>
-          )}
-          <Image src={dot} alt="dot" height={2} width={2} />
-
-          {hydrated ? (
-            <time
-              dateTime={comment.updatedAt.toISOString()}
-              title={comment.updatedAt.toLocaleDateString("hr-HR")}
-              className="text-zinc-500"
-            >
-              {getRelativeTimeString(comment.updatedAt)}
-            </time>
-          ) : (
-            <span className="text-zinc-500">Time in progress...</span>
-          )}
-        </div>
+        <CommentMetadata />
         <div className="ml-3 flex flex-col gap-4 border-l-2 border-zinc-700/70 pl-2">
           <div className="flex flex-col gap-1 pl-2">
             <CommentContentRTE editable={editable} cancelEdit={cancelEdit} />
---
+++ components/comment/CommentMetadata.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { useCommentContext } from "@/lib/context/CommentContextProvider";
+import useHydration from "@/lib/hooks/useHydration";
+import getRelativeTimeString from "@/lib/utils/getRelativeTimeString";
+import dot from "@/public/dot.svg";
+
+export default function CommentMetadata() {
+  const hydrated = useHydration();
+  const comment = useCommentContext();
+
+  return (
+    <div className="flex items-center gap-1 text-xs">
+      <Link href={`/u/${comment.author.name}`} className="rounded-full">
+        <Image
+          src={comment.author.imageUrl}
+          alt="user background"
+          priority
+          width={28}
+          height={28}
+          className="rounded-full"
+        />
+      </Link>
+      <Link
+        href={`/u/${comment.author.name}`}
+        className="font-medium hover:underline"
+      >
+        {comment.author.name}
+      </Link>
+
+      {comment.authorId === comment.post.authorId && (
+        <div className="font-bold uppercase text-blue-500">op</div>
+      )}
+      <Image src={dot} alt="dot" height={2} width={2} />
+
+      {hydrated ? (
+        <>
+          <time
+            dateTime={comment.createdAt.toISOString()}
+            title={comment.createdAt.toLocaleDateString("hr-HR")}
+            className="text-zinc-500"
+          >
+            {getRelativeTimeString(comment.createdAt)}
+          </time>
+          {comment.updatedAt > comment.createdAt && (
+            <>
+              <Image src={dot} alt="dot" height={2} width={2} />
+              <time
+                dateTime={comment.updatedAt.toISOString()}
+                title={comment.updatedAt.toLocaleDateString("hr-HR")}
+                className="italic text-zinc-500"
+              >
+                edited {getRelativeTimeString(comment.updatedAt)}
+              </time>
+            </>
+          )}
+        </>
+      ) : (
+        <span className="text-zinc-500">Time in progress...</span>
+      )}
+    </div>
+  );
+}
