210bd62fbc1085a554350ec6111c5283464d3a9d
Leonardo Yakub <leonardo.yakub@gmail.com>
2024-02-08 13:00:40 +0100

    added post content to the loading state of the RTE if there is any

--- components/RTE/PostRTELoading.tsx
+++ components/RTE/PostRTELoading.tsx
@@ -1,8 +1,22 @@
-export default function PostRTELoading() {
+import { Post } from "@/lib/db/schema";
+
+export default function PostRTELoading({
+  content,
+}: {
+  content?: Post["text"];
+}) {
   return (
     <div className="animate-pulse rounded border border-zinc-700/70">
       <div className="h-10 rounded-t bg-zinc-800" />
-      <div className="prose prose-sm prose-zinc prose-invert min-h-[8rem] max-w-none px-4 py-2 focus:outline-none" />
+      {content && (
+        <div
+          className="prose prose-sm prose-zinc prose-invert min-h-[8rem] max-w-none px-4 py-2 focus:outline-none"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      )}
+      {!content && (
+        <div className="prose prose-sm prose-zinc prose-invert min-h-[8rem] max-w-none px-4 py-2 focus:outline-none" />
+      )}
     </div>
   );
 }
