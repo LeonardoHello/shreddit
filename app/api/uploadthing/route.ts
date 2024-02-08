eae2682764001ac3458f1bbaeb7dab66e9e230cd
Leonardo Yakub <leonardo.yakub@gmail.com>
2024-02-08 13:06:30 +0100

    added edge runtime to uploadthing api route

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -2,6 +2,7 @@ import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
+export const runtime = "edge";
 export const preferredRegion = ["fra1"];
 
 // Export routes for Next App Router
