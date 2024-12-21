e1fc17549c2ada011304c97794054ce45ff1db9c
Leonardo <leonardo.yakub@gmail.com>
2024-12-21 04:00:52 +0100

    adjusted middleware protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,11 @@ import { NextResponse } from "next/server";
 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/home(.*)", "/submit(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/home(.*)",
+  "/submit(.*)",
+  "/r/(.*)/submit(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
