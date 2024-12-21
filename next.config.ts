cdae5ef437bdac226ce695f8f9473463c33b4936
Leonardo <leonardo.yakub@gmail.com>
2024-12-21 07:57:16 +0100

    using experimental react compiler

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
   images: {
     remotePatterns: [
       { hostname: "img.clerk.com", protocol: "https", port: "" },
