a84f6cb70127fc03611866425f88fd9f4366e501
Leonardo <leonardo.yakub@gmail.com>
2024-12-28 05:34:57 +0100

    conditionally rendering moderated communities

--- src/components/user/UserInfo.tsx
+++ src/components/user/UserInfo.tsx
@@ -80,38 +80,41 @@ export default function UserInfo({ user }: { user: user }) {
         <div />
       </div>
 
-      <hr className="border-zinc-700/70" />
+      {user.communities.length > 0 && (
+        <>
+          <hr className="border-zinc-700/70" />
+          <div>
+            <h1 className="mb-4 max-w-[15rem] font-bold tracking-wide text-zinc-500">
+              Moderator of these communities
+            </h1>
+            <div className="flex h-56 flex-col gap-3.5 overflow-y-scroll">
+              {user.communities.map((community) => (
+                <div key={community.id} className="flex items-center gap-1.5">
+                  <CommunityImage imageUrl={community.imageUrl} size={32} />
 
-      <div>
-        <h1 className="mb-4 max-w-[15rem] font-bold tracking-wide text-zinc-500">
-          Moderator of these communities
-        </h1>
-        <div className="flex h-56 flex-col gap-3.5 overflow-y-scroll">
-          {user.communities.map((community) => (
-            <div key={community.id} className="flex items-center gap-1.5">
-              <CommunityImage imageUrl={community.imageUrl} size={32} />
-
-              <div className="truncate text-xs tracking-wide">
-                <Link
-                  href={`/r/${community.name}`}
-                  className="max-w-[15rem] cursor-pointer font-medium lowercase hover:underline"
-                >
-                  r/{community.name}
-                </Link>
-                <div>
-                  {new Intl.NumberFormat("en-US", {
-                    notation: "compact",
-                    maximumFractionDigits: 1,
-                  }).format(community.usersToCommunities.length)}{" "}
-                  {community.usersToCommunities.length === 1
-                    ? "member"
-                    : "members"}
+                  <div className="truncate text-xs tracking-wide">
+                    <Link
+                      href={`/r/${community.name}`}
+                      className="max-w-[15rem] cursor-pointer font-medium lowercase hover:underline"
+                    >
+                      r/{community.name}
+                    </Link>
+                    <div>
+                      {new Intl.NumberFormat("en-US", {
+                        notation: "compact",
+                        maximumFractionDigits: 1,
+                      }).format(community.usersToCommunities.length)}{" "}
+                      {community.usersToCommunities.length === 1
+                        ? "member"
+                        : "members"}
+                    </div>
+                  </div>
                 </div>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
