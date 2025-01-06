d2b28612c8b84d2e20e7a559740678d0dc736f72
Leonardo <leonardo.yakub@gmail.com>
2025-01-06 14:18:25 +0100

    adjusting Sidebar menu skeleton loader

--- src/components/sidebar/SidebarMenuSkeleton.tsx
+++ src/components/sidebar/SidebarMenuSkeleton.tsx
@@ -1,15 +1,19 @@
 import { Skeleton } from "../ui/skeleton";
 
 export default function SidebarMenuSkeleton({
-  length = 4,
+  length = 5,
 }: {
   length?: number;
 }) {
   return (
-    <menu className="flex flex-col gap-2.5">
+    <menu>
       {Array.from({ length }).map((_, index) => (
-        <li key={index}>
-          <Skeleton className="h-8" />
+        <li key={index} className="flex h-10 items-center gap-2 px-4">
+          <Skeleton className="size-8 rounded-full" />
+          <Skeleton
+            className="h-4"
+            style={{ width: `${60 + ((index * 20) % 60)}px` }}
+          />
         </li>
       ))}
     </menu>
