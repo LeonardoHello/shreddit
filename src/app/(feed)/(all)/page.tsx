1b493975634f1acfffbd9342ca593b198bc0c573
Leonardo <leonardo.yakub@gmail.com>
2025-01-12 06:09:27 +0100

    prefetching content for home and all page

--- src/app/(feed)/(all)/page.tsx
+++ src/app/(feed)/(all)/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
+import { Suspense } from "react";
 
 import { auth as authPromise } from "@clerk/nextjs/server";
-import { ChartBarIcon } from "@heroicons/react/24/solid";
 
 import FeedPostInfiniteQuery from "@/components/feed/FeedPostInfiniteQuery";
+import FeedPostInfiniteQuerySkeleton from "@/components/feed/FeedPostInfiniteQuerySkeleton";
 import FeedSort from "@/components/feed/FeedSort";
-import PremiumButton from "@/components/sidebar/PremiumButton";
-import ScrollToTop from "@/components/sidebar/ScrollToTop";
+import { HydrateClient, trpc } from "@/trpc/server";
 import { PostSort } from "@/types";
 
 export const runtime = "edge";
@@ -21,64 +19,29 @@ export default async function AllPage(props: {
     authPromise(),
   ]);
 
+  void trpc.postFeed.getAllPosts.prefetchInfinite({
+    sort: searchParams.sort,
+  });
+
   return (
-    <div className="container mx-auto grid grid-cols-1 grid-rows-[auto,minmax(0,1fr)] gap-6 px-2 py-4 lg:grid-cols-[minmax(0,1fr),20rem] lg:pb-12 xl:max-w-6xl">
-      <div className="flex flex-col gap-2.5">
+    <div className="container flex gap-6 p-2 2xl:max-w-[1080px]">
+      <div className="flex grow flex-col gap-4">
         <FeedSort />
-      </div>
-
-      <div className="row-span-2 hidden max-w-80 flex-col gap-4 text-sm lg:flex">
-        <PremiumButton />
-        <div className="sticky top-4 flex flex-col gap-3 rounded border border-zinc-700/70 bg-zinc-900 p-3 pt-2">
-          <Image
-            src="https://www.redditstatic.com/desktop2x/img/id-cards/banner@2x.png"
-            alt="banner"
-            priority
-            width={624}
-            height={68}
-            className="absolute left-0 top-0 h-8 rounded-t object-cover"
-          />
-          <div className="z-10 mt-4 flex items-center gap-2">
-            <ChartBarIcon
-              className="h-7 w-7 rounded-full bg-zinc-300 stroke-[3] p-0.5 text-zinc-900"
-              width={20}
-              height={20}
+        <HydrateClient>
+          <Suspense fallback={<FeedPostInfiniteQuerySkeleton />}>
+            <FeedPostInfiniteQuery
+              currentUserId={auth.userId}
+              infiniteQueryOptions={{
+                procedure: "getAllPosts",
+                input: {
+                  sort: searchParams.sort,
+                },
+              }}
             />
-            <h1>All</h1>
-          </div>
-          <p className="text-sm">
-            The most active posts from all of Shreddit. Come here to see new
-            posts rising and be a part of the conversation.
-          </p>
-          <hr className="border-zinc-700/70" />
-          <Link href="/submit" className="rounded-full">
-            <button className="w-full rounded-full bg-zinc-300 p-1.5 text-sm font-bold text-zinc-900 transition-colors hover:bg-zinc-400">
-              Create Post
-            </button>
-          </Link>
-          <Link
-            href={{ query: { submit: "community" } }}
-            scroll={false}
-            className="rounded-full"
-          >
-            <button className="w-full rounded-full border border-zinc-300 p-1.5 text-sm font-bold text-zinc-300 transition-colors hover:bg-zinc-800">
-              Create Community
-            </button>
-          </Link>
-        </div>
-        <ScrollToTop />
+          </Suspense>
+        </HydrateClient>
       </div>
-
-      <FeedPostInfiniteQuery
-        key={searchParams.sort}
-        currentUserId={auth.userId}
-        queryInfo={{
-          procedure: "getAllPosts",
-          input: {
-            sort: searchParams.sort,
-          },
-        }}
-      />
+      <div className="hidden w-80 lg:block" />
     </div>
   );
 }
--- src/app/(feed)/home/page.tsx
+++ src/app/(feed)/home/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
+import { Suspense } from "react";
 
 import { auth as authPromise } from "@clerk/nextjs/server";
-import { HomeIcon } from "@heroicons/react/24/solid";
 
 import FeedPostInfiniteQuery from "@/components/feed/FeedPostInfiniteQuery";
+import FeedPostInfiniteQuerySkeleton from "@/components/feed/FeedPostInfiniteQuerySkeleton";
 import FeedSort from "@/components/feed/FeedSort";
-import PremiumButton from "@/components/sidebar/PremiumButton";
-import ScrollToTop from "@/components/sidebar/ScrollToTop";
+import { HydrateClient, trpc } from "@/trpc/server";
 import { PostSort } from "@/types";
 
 export const runtime = "edge";
@@ -24,60 +22,27 @@ export default async function HomePage(props: {
   if (auth.userId === null)
     throw new Error("Could not load home page information.");
 
+  void trpc.postFeed.getHomePosts.prefetchInfinite({ sort: searchParams.sort });
+
   return (
-    <div className="container mx-auto grid grid-cols-1 grid-rows-[auto,minmax(0,1fr)] gap-6 px-2 py-4 lg:grid-cols-[minmax(0,1fr),20rem] lg:pb-12 xl:max-w-6xl">
-      <div className="flex flex-col gap-2.5">
+    <div className="container flex gap-6 p-2 pb-6 2xl:max-w-[1080px]">
+      <div className="flex grow flex-col gap-4">
         <FeedSort />
+        <HydrateClient>
+          <Suspense fallback={<FeedPostInfiniteQuerySkeleton />}>
+            <FeedPostInfiniteQuery
+              currentUserId={auth.userId}
+              infiniteQueryOptions={{
+                procedure: "getHomePosts",
+                input: {
+                  sort: searchParams.sort,
+                },
+              }}
+            />
+          </Suspense>
+        </HydrateClient>
       </div>
-
-      <div className="row-span-2 hidden max-w-80 flex-col gap-4 text-sm lg:flex">
-        <PremiumButton />
-        <div className="sticky top-4 flex flex-col gap-3 rounded border border-zinc-700/70 bg-zinc-900 p-3 pt-2">
-          <Image
-            src="https://www.redditstatic.com/desktop2x/img/id-cards/home-banner@2x.png"
-            alt="home banner"
-            priority
-            width={624}
-            height={68}
-            className="absolute left-0 top-0 h-8 rounded-t object-cover"
-          />
-          <div className="z-10 mt-4 flex items-center gap-2">
-            <HomeIcon className="h-7 w-7" />
-            <h1>Home</h1>
-          </div>
-          <p className="text-sm">
-            Your personal Shreddit frontpage. Come here to check in with your
-            favorite communities.
-          </p>
-          <hr className="border-zinc-700/70" />
-          <Link href="/submit" className="rounded-full">
-            <button className="w-full rounded-full bg-zinc-300 p-1.5 text-sm font-bold text-zinc-900 transition-colors hover:bg-zinc-400">
-              Create Post
-            </button>
-          </Link>
-          <Link
-            href={{ query: { submit: "community" } }}
-            scroll={false}
-            className="rounded-full"
-          >
-            <button className="w-full rounded-full border border-zinc-300 p-1.5 text-sm font-bold text-zinc-300 transition-colors hover:bg-zinc-800">
-              Create Community
-            </button>
-          </Link>
-        </div>
-        <ScrollToTop />
-      </div>
-
-      <FeedPostInfiniteQuery
-        key={searchParams.sort}
-        currentUserId={auth.userId}
-        queryInfo={{
-          procedure: "getHomePosts",
-          input: {
-            sort: searchParams.sort,
-          },
-        }}
-      />
+      <div className="hidden w-80 lg:block" />
     </div>
   );
 }
