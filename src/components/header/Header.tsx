4a6e3ed746774ebc316c5177c09eb12990f89b9e
Leonardo <leonardo.yakub@gmail.com>
2025-01-13 07:27:07 +0100

    added suspense to component fetching auth

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Link from "next/link";
 
 import {
@@ -17,71 +18,79 @@ import { Skeleton } from "../ui/skeleton";
 import HeaderSheet from "./HeaderSheet";
 
 export default async function Header() {
-  const { userId } = await auth();
-
   return (
     <header className="sticky top-0 z-20 col-span-2 flex h-14 justify-between gap-5 border-b bg-card px-4 py-2">
       <div className="flex select-none items-center gap-1.5">
         <HeaderSheet>
-          <Sidebar sheet />
+          <Sidebar />
         </HeaderSheet>
 
         <Logo />
       </div>
+
       <Search />
 
-      <div className="flex items-center gap-2 self-center">
-        {userId && (
-          <>
-            <Button
-              variant={"ghost"}
-              size={"icon"}
-              className="rounded-full md:hidden"
-              asChild
-            >
-              <Link href={"/submit"}>
-                <Plus className="size-7 stroke-1" />
-              </Link>
-            </Button>
-            <Button
-              variant={"ghost"}
-              className="hidden gap-1 rounded-full pl-2.5 pr-3.5 md:flex"
-              asChild
-            >
-              <Link href={"/submit"}>
-                <Plus className="size-7 stroke-1" />
-                Create
-              </Link>
-            </Button>
-
-            <ClerkLoading>
-              <Skeleton className="size-8 rounded-full" />
-            </ClerkLoading>
-            <ClerkLoaded>
-              <UserButton
-                appearance={{ elements: { userButtonAvatarBox: "size-8" } }}
-              />
-            </ClerkLoaded>
-          </>
-        )}
-
-        {!userId && (
-          <>
-            <ClerkLoading>
-              <Button variant={"secondary"} className="rounded-full">
-                Sign in
-              </Button>
-            </ClerkLoading>
-            <ClerkLoaded>
-              <SignInButton mode="modal">
-                <Button variant={"secondary"} className="rounded-full">
-                  Sign in
-                </Button>
-              </SignInButton>
-            </ClerkLoaded>
-          </>
-        )}
-      </div>
+      <Suspense fallback={<Skeleton className="size-8 rounded-full" />}>
+        <HeaderUserProfile />
+      </Suspense>
     </header>
+  );
+}
+
+async function HeaderUserProfile() {
+  const { userId } = await auth();
+
+  if (!userId)
+    return (
+      <div className="flex items-center gap-2 self-center">
+        <ClerkLoading>
+          <Button variant={"secondary"} className="rounded-full">
+            Sign in
+          </Button>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignInButton mode="modal">
+            <Button variant={"secondary"} className="rounded-full">
+              Sign in
+            </Button>
+          </SignInButton>
+        </ClerkLoaded>
+      </div>
+    );
+
+  return (
+    <div className="flex items-center gap-2 self-center">
+      {/* mobile */}
+      <Button
+        variant={"ghost"}
+        size={"icon"}
+        className="rounded-full md:hidden"
+        asChild
+      >
+        <Link href={"/submit"}>
+          <Plus className="size-7 stroke-1" />
+        </Link>
+      </Button>
+      {/* desktop */}
+      <Button
+        variant={"ghost"}
+        className="hidden gap-1 rounded-full pl-2.5 pr-3.5 md:flex"
+        asChild
+      >
+        <Link href={"/submit"}>
+          <Plus className="size-7 stroke-1" />
+          Create
+        </Link>
+      </Button>
+
+      <ClerkLoading>
+        <Skeleton className="size-8 rounded-full" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <UserButton
+          appearance={{ elements: { userButtonAvatarBox: "size-8" } }}
+        />
+      </ClerkLoaded>
+    </div>
   );
 }
