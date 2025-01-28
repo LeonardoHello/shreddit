0f19fdc349b033e1717136579af5a4a7652fa65f
Leonardo <leonardo.yakub@gmail.com>
2025-01-28 23:07:14 +0100

    adjustign post skeleton loader

--- src/components/post/PostSkeleton.tsx
+++ src/components/post/PostSkeleton.tsx
@@ -1,19 +1,26 @@
-import { ArrowDownCircle, ArrowUpCircle } from "lucide-react";
-
 import { Skeleton } from "../ui/skeleton";
 
 export default function PostSkeleton() {
   return (
-    <div className="flex w-full gap-3 rounded border bg-card p-2">
-      <div className="flex select-none flex-col items-center gap-1 text-muted-foreground">
-        <ArrowUpCircle className="size-6 rounded" />
-        <Skeleton className="h-3 w-4" />
-        <ArrowDownCircle className="size-6 rounded" />
+    <div className="flex w-full flex-col gap-3 rounded border bg-card px-4 py-2">
+      <div className="flex items-center justify-between">
+        <div className="flex w-full items-center gap-2">
+          <Skeleton className="size-8 rounded-full" />
+          <div className="flex flex-col gap-1">
+            <Skeleton className="h-3 w-32" />
+            <Skeleton className="h-3 w-16" />
+          </div>
+        </div>
+        <Skeleton className="size-7 rounded-full" />
       </div>
-      <div className="flex grow flex-col gap-2">
-        <Skeleton className="mb-2 h-3 w-2/5" />
-        <Skeleton className="h-4 w-1/2" />
-        <Skeleton className="h-96" />
+      <div className="flex grow flex-col gap-3">
+        <Skeleton className="h-7 w-1/2" />
+        <Skeleton className="h-80" />
+      </div>
+      <div className="flex gap-2">
+        <Skeleton className="h-8 w-[4.5rem] rounded-full" />
+        <Skeleton className="h-8 w-14 rounded-full" />
+        <Skeleton className="h-8 w-20 rounded-full" />
       </div>
     </div>
   );
