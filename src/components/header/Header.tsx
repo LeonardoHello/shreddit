edb314dd833b94c639b0ae7c974ef5be8083443e
Leonardo <leonardo.yakub@gmail.com>
2025-02-04 20:21:20 +0100

    passing userId to the Logo component instead of using a hook

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,11 +3,10 @@ import Link from "next/link";
 import {
   ClerkLoaded,
   ClerkLoading,
-  SignedIn,
-  SignedOut,
   SignInButton,
   UserButton,
 } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { Plus } from "lucide-react";
 
 import Logo from "@/components/header/Logo";
@@ -15,19 +14,25 @@ import Search from "@/components/header/Search";
 import { Button } from "../ui/button";
 import { Skeleton } from "../ui/skeleton";
 
-export default function Header({ children }: { children: React.ReactNode }) {
+export default async function Header({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = await auth();
+
   return (
     <header className="sticky top-0 z-20 col-span-2 flex h-14 justify-between gap-5 border-b bg-card px-4 py-2">
       <div className="flex select-none items-center gap-1.5">
         {/* sidebar */}
         {children}
 
-        <Logo />
+        <Logo userId={userId} />
       </div>
 
       <Search />
 
-      <SignedIn>
+      {userId && (
         <div className="flex items-center gap-2 self-center">
           <Button
             variant={"ghost"}
@@ -49,9 +54,9 @@ export default function Header({ children }: { children: React.ReactNode }) {
             />
           </ClerkLoaded>
         </div>
-      </SignedIn>
+      )}
 
-      <SignedOut>
+      {!userId && (
         <div className="flex items-center gap-2 self-center">
           <ClerkLoading>
             <Button className="rounded-full bg-rose-600 text-foreground hover:bg-rose-600/90">
@@ -66,7 +71,7 @@ export default function Header({ children }: { children: React.ReactNode }) {
             </SignInButton>
           </ClerkLoaded>
         </div>
-      </SignedOut>
+      )}
     </header>
   );
 }
--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,23 +1,23 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 
-import { useAuth } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/server";
 
 import logo from "@public/logo.svg";
 import logoText from "@public/logoText.svg";
 
-export default function Logo() {
-  // TODO: remove this
-  const auth = useAuth();
-
-  const href = auth.isSignedIn ? "/home" : "/";
+export default function Logo({ userId }: { userId: User["id"] | null }) {
+  const href = userId ? "/home" : "/";
 
   return (
     <Link href={href} className="flex items-center gap-1.5">
-      <Image src={logo} alt="logo" className="size-8" />
-      <Image src={logoText} alt="logo" className="hidden h-6 w-auto md:block" />
+      <Image src={logo} alt="logo" className="size-8" priority />
+      <Image
+        src={logoText}
+        alt="logo"
+        className="hidden h-6 w-auto md:block"
+        priority
+      />
     </Link>
   );
 }
