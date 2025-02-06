c27134b7551cdc352dc3e7c7ba625b1e055a5f19
Leonardo <leonardo.yakub@gmail.com>
2025-02-06 08:44:02 +0100

    replaced toast sonner library with shadcn-s wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,13 @@ import { Reddit_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
-import { Toaster } from "sonner";
 import { extractRouterConfig } from "uploadthing/server";
 
 import { ourFileRouter } from "@/app/api/uploadthing/core";
 import Header from "@/components/header/Header";
 import Sidebar from "@/components/sidebar/Sidebar";
 import SidebarSheet from "@/components/sidebar/SidebarSheet";
+import { Toaster } from "@/components/ui/sonner";
 import RecentCommunityContextProvider from "@/context/RecentCommunityContext";
 import { TRPCProvider } from "@/trpc/client";
 
@@ -74,7 +74,7 @@ export default function RootLayout({
               </div>
             </RecentCommunityContextProvider>
           </TRPCProvider>
-          <Toaster theme="dark" richColors closeButton />
+          <Toaster closeButton />
         </body>
       </html>
     </ClerkProvider>
