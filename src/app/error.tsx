82e19defcf8c12f756883eaa8ac682d9ffb4046d
Leonardo <leonardo.yakub@gmail.com>
2025-02-09 16:10:18 +0100

    updated error pages styling

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,6 +2,19 @@
 
 import Link from "next/link";
 
+import { TRPCClientError } from "@trpc/client";
+import { ArrowRight } from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { AppRouter } from "@/trpc/routers/_app";
+
+function isTRPCClientError(
+  cause: unknown,
+): cause is TRPCClientError<AppRouter> {
+  return cause instanceof TRPCClientError;
+}
+
 export default function Error({
   error,
   reset,
@@ -9,28 +22,62 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  if (isTRPCClientError(error)) {
+    const { message, data } = error;
+
+    return (
+      <main className="grid grow place-items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <Badge variant="secondary" className="mb-2">
+            {data ? data.httpStatus : "4xx"}
+          </Badge>
+
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+            {data ? data.code.replaceAll("_", " ") : "CLIENT ERROR"}
+          </h1>
+
+          <p className="mt-6 font-semibold leading-7 text-muted-foreground sm:text-lg">
+            {message}
+          </p>
+
+          <div className="mt-10 flex flex-wrap items-center justify-center gap-6">
+            <Button onClick={reset}>Try again</Button>
+            <Button variant="link" asChild className="group">
+              <Link href="/">
+                Go back home{" "}
+                <ArrowRight className="size-4 transition-transform group-hover:translate-x-1" />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main className="grid grow place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-rose-500">
-          Oops! Something Went Wrong
-        </p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-200 sm:text-6xl">
-          {error.message}
+        <Badge variant="secondary" className="mb-2">
+          5xx
+        </Badge>
+
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+          INTERNAL SERVER ERROR
         </h1>
-        <p className="mt-6 text-base leading-7 text-gray-400">
-          Please try again later or contact support if the problem persists.
+
+        <p className="mt-6 font-semibold leading-7 text-muted-foreground sm:text-lg">
+          An unexpected error occurred while processing your request. Please try
+          again later.
         </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <button
-            onClick={reset}
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Try again
-          </button>
-          <Link href="/" className="text-sm font-semibold text-gray-200">
-            Go back home <span aria-hidden="true">&rarr;</span>
-          </Link>
+
+        <div className="mt-10 flex flex-wrap items-center justify-center gap-6">
+          <Button onClick={reset}>Try again</Button>
+          <Button variant="link" asChild className="group">
+            <Link href="/">
+              Go back home{" "}
+              <ArrowRight className="size-4 transition-transform group-hover:translate-x-1" />
+            </Link>
+          </Button>
         </div>
       </div>
     </main>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,33 @@
-"use client";
-
 import Link from "next/link";
+
+import { ArrowRight } from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
     <main className="grid grow place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-rose-500">404 Error</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-200 sm:text-6xl">
-          Page Not Found
+        <Badge variant="secondary" className="mb-2">
+          404
+        </Badge>
+
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+          PAGE NOT FOUND
         </h1>
-        <p className="mt-6 text-base leading-7 text-gray-400">
-          The page you are looking for does not exist or has been moved.
+
+        <p className="mt-6 font-semibold leading-7 text-muted-foreground sm:text-lg">
+          The page you are looking for does not exist.
         </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Link href="/" className="text-sm font-semibold text-gray-200">
-            Go back home <span aria-hidden="true">&rarr;</span>
-          </Link>
+
+        <div className="mt-10 flex flex-wrap items-center justify-center gap-6">
+          <Button variant="link" asChild className="group">
+            <Link href="/">
+              Go back home{" "}
+              <ArrowRight className="size-4 transition-transform group-hover:translate-x-1" />
+            </Link>
+          </Button>
         </div>
       </div>
     </main>
