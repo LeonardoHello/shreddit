2c7d34ee4c4c72f1276d10a18f4033c482e81a36
Leonardo <leonardo.yakub@gmail.com>
2025-04-07 04:25:19 +0200

    removed suspense from prefetched layout content

--- src/app/r/[communityName]/layout.tsx
+++ src/app/r/[communityName]/layout.tsx
@@ -1,12 +1,8 @@
-import { Suspense } from "react";
-
 import { auth as authPromise } from "@clerk/nextjs/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import CommunityHeader from "@/components/community/CommunityHeader";
-import CommunityHeaderSkeleton from "@/components/community/CommunityHeaderSkeleton";
 import CommunitySidebar from "@/components/community/CommunitySidebar";
-import CommunitySidebarSkeleton from "@/components/community/CommunitySidebarSkeleton";
 import { getQueryClient, trpc } from "@/trpc/server";
 
 export default async function CommunityLayout(props: {
@@ -30,20 +26,16 @@ export default async function CommunityLayout(props: {
   return (
     <div className="container flex grow flex-col gap-4 p-2 pb-6 lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl">
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="order-2 flex justify-center gap-4">
+        <CommunityHeader
+          currentUserId={auth.userId}
+          communityName={params.communityName}
+        />
+
+        <div className="flex justify-center gap-4">
           {props.children}
 
-          <Suspense fallback={<CommunitySidebarSkeleton />}>
-            <CommunitySidebar communityName={params.communityName} />
-          </Suspense>
+          <CommunitySidebar communityName={params.communityName} />
         </div>
-
-        <Suspense fallback={<CommunityHeaderSkeleton />}>
-          <CommunityHeader
-            currentUserId={auth.userId}
-            communityName={params.communityName}
-          />
-        </Suspense>
       </HydrationBoundary>
     </div>
   );
--- src/app/u/[username]/layout.tsx
+++ src/app/u/[username]/layout.tsx
@@ -1,11 +1,7 @@
-import { Suspense } from "react";
-
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import UserHeader from "@/components/user/UserHeader";
-import UserHeaderSkeleton from "@/components/user/UserHeaderSkeleton";
 import UserSidebar from "@/components/user/UserSidebar";
-import UserSidebarSkeleton from "@/components/user/UserSidebarSkeleton";
 import { getQueryClient, trpc } from "@/trpc/server";
 
 export default async function UserLayout(props: {
@@ -23,17 +19,13 @@ export default async function UserLayout(props: {
   return (
     <div className="container flex grow flex-col gap-4 p-2 pb-6 lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl">
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="order-2 flex justify-center gap-4">
+        <UserHeader username={params.username} />
+
+        <div className="flex justify-center gap-4">
           {props.children}
 
-          <Suspense fallback={<UserSidebarSkeleton />}>
-            <UserSidebar username={params.username} />
-          </Suspense>
+          <UserSidebar username={params.username} />
         </div>
-
-        <Suspense fallback={<UserHeaderSkeleton />}>
-          <UserHeader username={params.username} />
-        </Suspense>
       </HydrationBoundary>
     </div>
   );
