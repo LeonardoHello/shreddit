95c05c982bd569168f8b15c3c1b3fa9d737fcc1e
Leonardo <leonardo.yakub@gmail.com>
2025-11-27 04:28:37 +0100

    removed shrek logo :(

--- src/app/(feedLayout)/sign-in/page.tsx
+++ src/app/(feedLayout)/sign-in/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import SignInButtons from "@/components/sign-in-buttons";
 import {
   Card,
@@ -8,12 +6,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import shrek from "@public/shrek.svg";
 
 export default function SignInPage() {
   return (
     <div className="container flex grow flex-col items-center justify-center gap-4 p-2 pb-6 lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl">
-      <Image src={shrek} alt="shrek" />
       <Card>
         <CardHeader>
           <CardTitle>Welcome</CardTitle>
